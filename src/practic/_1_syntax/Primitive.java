2ed26bfbf8fa79fb1b7a813320a56e569b51d34a
Jami-Kaz <kazbulatovajam@gmail.com>
2022-08-18 11:52:33 +0500

    add Bool.java, ArrAndString.java edit Primitive.java

--- src/practic/_1_syntax/Primitive.java
+++ src/practic/_1_syntax/Primitive.java
@@ -4,15 +4,19 @@ class Primitive {
 
     public static void main(String[] args) {
 
-        byte    b = 127 - 128;
-        short   s = 32540 - 32000;
-        char    c = 'h';
-        int     i = 2_100_000_000 - 2_000_000_000;
-        long    l = 9_100_100_000_000L - 9_000_000_000_000L;
-        float   f = 999914445.992f - 99994415.992f;
-        double  d = 1266655555.4 - 1266655555.4;
+        byte b = 127 - 128;          // 8
+        short s = -32000;            // 16
+        char c = 65000;              // 16
 
-        boolean bo = c > 'h';
+        int i = 2_000_000_000;       // 32
+        float f = 2.9f;              // 32
+
+        long l = 9_000_000_000_000L; // 64
+        double d = 9.9d;             // 64
+
+        boolean bo = true == false;
+
+        bo = c > 'h';
 
         char   cha = 65000;
         double dou = l;
